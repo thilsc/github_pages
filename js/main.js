45c5f05d9f05cfddc701bff2f46bb09d1aa28064
Thiago Coutinho <thilsc@users.noreply.github.com>
2020-08-20 14:38:23 -0300

    Criando function de cálculo da idade

--- js/main.js
+++ js/main.js
@@ -131,3 +131,15 @@
     "use strict";
     $('#loader').fadeOut();
    });
+
+
+function idade(nascimento, hoje) {
+
+    var diferencaAnos = hoje.getFullYear() - nascimento.getFullYear();
+
+    if ( new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate()) < 
+         new Date(hoje.getFullYear(), nascimento.getMonth(), nascimento.getDate()) )
+        diferencaAnos--;
+
+    return diferencaAnos;
+}
